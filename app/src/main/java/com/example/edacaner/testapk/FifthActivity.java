3c9a3355cb1ef4732d3a2c330c36a6e7cadaf27f
Ufuk Akgeyik <ufukakgeyik@sabanciuniv.edu>
2018-04-22 19:54:40 +0300

    Spinners and radiobuttons actions are fixed

--- app/src/main/java/com/example/edacaner/testapk/FifthActivity.java
+++ app/src/main/java/com/example/edacaner/testapk/FifthActivity.java
@@ -1,8 +1,8 @@
 package com.example.edacaner.testapk;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -23,6 +23,7 @@ public class FifthActivity extends AppCompatActivity {
                 openSixthActivity();
             }
         });
+
 
     }
 
--- app/src/main/java/com/example/edacaner/testapk/SecondActivity.java
+++ app/src/main/java/com/example/edacaner/testapk/SecondActivity.java
@@ -1,66 +1,56 @@
 package com.example.edacaner.testapk;
 
 import android.content.Intent;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SecondActivity extends AppCompatActivity {
 
     private Button goBtn;
     private RadioButton flightBtn, accomodationBtn, eventBtn, changeBtn;
+    private RadioGroup radioGroupPurpose;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        radioGroupPurpose = findViewById(R.id.radioGroupPurpose);
         goBtn = findViewById(R.id.goButton);
+
         flightBtn = findViewById(R.id.radioButton);
         accomodationBtn = findViewById(R.id.radioButton2);
         eventBtn = findViewById(R.id.radioButton3);
         changeBtn = findViewById(R.id.radioButton4);
-
-
 
         goBtn.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
 
-                boolean flight = findViewById(R.id.radioButton).isSelected();
-                boolean accomodation = findViewById(R.id.radioButton2).isSelected();
-                boolean event = findViewById(R.id.radioButton3).isSelected();
-                boolean change = findViewById(R.id.radioButton4).isSelected();
-
-                if(flight)
-                {
-                    openEighthActivity();
-                }
-                else if(accomodation)
-                {
-                    openTwelfthActivity();
-                }
-                else if(event)
-                {
-                    openFifthActivity();
-                }
-                else if(change)
-                {
-                    openFourththActivity();
-                }
-                else
-                {
-                    openFifthActivity();
+                switch (getCheckedRadioButton()){
+                    case R.id.radioButton:
+                        openEighthActivity();
+                        break;
+                    case R.id.radioButton2:
+                        openTwelfthActivity();
+                        break;
+                    case R.id.radioButton3:
+                        openFifthActivity();
+                        break;
+                    case R.id.radioButton4:
+                        openFourthActivity();
+                        break;
                 }
             }
         });
 
     }
-    public void openFourththActivity() {
+    public void openFourthActivity() {
         Intent intent = new Intent(this, FourthActivity.class);
         startActivity(intent);
     }
@@ -78,6 +68,19 @@ public class SecondActivity extends AppCompatActivity {
     public void openTwelfthActivity() {
         Intent intent = new Intent(this, TwelfthActivity.class);
         startActivity(intent);
+    }
+
+    private int getCheckedRadioButton(){
+        int which = -1;
+        if (radioGroupPurpose.getCheckedRadioButtonId() == -1)
+        {
+            // no radio buttons are checked
+        }
+        else
+        {
+            which = radioGroupPurpose.getCheckedRadioButtonId();
+        }
+        return which;
     }
 
 }
